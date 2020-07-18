a6bc027268f74ebe4aed3049dca1809b912e7f0d
Christian Fries <christian.fries123@gmail.com>
2020-07-18 12:29:47 +0200

    update configs

--- professos/testplan/base.py
+++ professos/testplan/base.py
@@ -131,6 +131,7 @@ class BaseTest(object):
         result_status = result['Result']
         print(" - {}".format(result_status))
         if screenshot:
+            #print("{}".format(json.dumps(result, indent=4)))
             #print("{}".format(json.dumps(result['LogEntry'], indent=4)))
             cnt = 0
             for entry in result['LogEntry']:
