a7706d1a80ec35175601106933b2a94f07165f02
Christian Fries <christian.fries123@gmail.com>
2020-08-22 10:02:31 +0200

    load config

--- api/settings/testconfig.py
+++ api/settings/testconfig.py
@@ -7,21 +7,30 @@ class TestConfig(object):
     def __init__(self):
         self.discovery = True
         self.dynamic = True
+        self.pre_expose = False
+        self.test_id = ""
+        self.skip_tests = ""
+
 
     def loadJson(self, file):
         if not os.path.exists(file):
             return None
         with open(file, 'r') as jsonData:
             data = json.load(jsonData)
-            self.testId = data['testId']
-            self.skipTests = data['skipTests']
-            self.disable_dynamic = data['disable_dynamic']
+            self.test_id = data.get('test_id', "")
+            skipTests = data.get('skip_tests', "")
+            if skipTests != "":
+                self.skip_tests = [int(x) for x in skipTests.split(",")]
+            self.discovery = data.get('discovery', True)
+            self.dynamic = data.get('dynamic', True)
+            self.pre_expose = data.get('pre_expose', True)
 
 
 if __name__ == "__main__":
     cfg = TestConfig()
-    cfg.loadJson("../../config/rp/angular-auth-oidc/statictest.json")
+    cfg.loadJson("../../config/rp/angular-auth-oidc/config.json")
 
     print(cfg.discovery)
-    print(cfg.testId)
-    print(cfg.skipTests)
+    print(cfg.test_id)
+    print(cfg.skip_tests)
+    print(cfg.dynamic)
